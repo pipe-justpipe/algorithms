ec2e05458a0c581c86353bf23c880865e7fcc97d
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-07-08 11:40:50 +0100

    algorithm

--- main.js
+++ main.js
@@ -1,33 +1,35 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('Enter a sentence: ', (sentence) => {
-  let length = 0;
-  let wordCount = 0;
-  let vowelCount = 0;
-
-  for (let i = 0; i < sentence.length; i++) {
-    length++;
-
-    if (sentence[i] === ' ') {
-      wordCount++;
+function analyzeSentence(sentence) {
+    let length = 0;
+    let wordCount = 0;
+    let vowelCount = 0;
+  
+    for (let i = 0; i < sentence.length; i++) {
+      const char = sentence[i];
+      length++;
+  
+      if (char === ' ') {
+        wordCount++;
+      }
+  
+      const lowercaseChar = char.toLowerCase();
+      if (lowercaseChar === 'a' || lowercaseChar === 'e' || lowercaseChar === 'i' || lowercaseChar === 'o' || lowercaseChar === 'u') {
+        vowelCount++;
+      }
     }
-
-    const lowercaseChar = sentence[i].toLowerCase();
-    if (lowercaseChar === 'a' || lowercaseChar === 'e' || lowercaseChar === 'i' || lowercaseChar === 'o' || lowercaseChar === 'u') {
-      vowelCount++;
-    }
+  
+    wordCount++; // Account for the last word after the final space
+  
+    return {
+      length: length,
+      wordCount: wordCount,
+      vowelCount: vowelCount
+    };
   }
-
-  wordCount++; // Account for the last word after the final space
-
-  console.log('Length of the sentence:', length);
-  console.log('Number of words:', wordCount);
-  console.log('Number of vowels:', vowelCount);
-
-  rl.close();
-});
+  
+  // Example usage:
+  const sentence = "The quick brown fox jumps over the lazy dog.";
+  const analysis = analyzeSentence(sentence);
+  
+  console.log('Length of the sentence:', analysis.length);
+  console.log('Number of words:', analysis.wordCount);
+  console.log('Number of vowels:', analysis.vowelCount);
